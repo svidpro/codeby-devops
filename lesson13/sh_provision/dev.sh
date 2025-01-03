49c7dce6eb51c77c34e57eebd3534c79ac722216
Ivan Svidinskiy <icommerce777@gmail.com>
2025-01-03 22:00:37 +0300

    fix(lesson13): fixing startup error

--- lesson13/sh_provision/dev.sh
+++ lesson13/sh_provision/dev.sh
@@ -5,5 +5,5 @@ sudo apt-get update
 sudo cat /home/vagrant/.ssh/vagrant_vm_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 
 sudo apt-get -y install python3 python3-pip
-sudo apt-get -y install mysql-client mysql-server
-sudo -u vagrant pip install PyMySQL
+# sudo apt-get -y install mysql-client mysql-server
+# sudo -u vagrant pip install PyMySQL
