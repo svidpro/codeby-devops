aa2d6ff918696a82a2dc8ed0fd816338583a6d95
Ivan Svidinskiy <icommerce777@gmail.com>
2025-01-04 13:50:12 +0300

    feat(lesson9): fixing startup error

--- lesson9/sh_provision/server.sh
+++ lesson9/sh_provision/server.sh
@@ -19,11 +19,50 @@ sudo apt-get install -y mysql-server rsync
 sudo service mysql start
 sudo service mysql enable
 
+# Новый пользователь
+# MYSQL_USER="user"
+# MYSQL_PASSWORD=$(openssl rand -base64 12)
+MYSQL_ROOT_PASSWORD=$(openssl rand -base64 12)
+
+# mysql -u root -e "
+# CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# FLUSH PRIVILEGES;
+# "
+
+# Создание файла с доступами MySQL для user
+# CONFIG_FILE="/home/vagrant/.my.cnf"
+# echo "[client]" > "$CONFIG_FILE"
+# echo "user=${MYSQL_USER}" >> "$CONFIG_FILE"
+# echo "password=${MYSQL_PASSWORD}" >> "$CONFIG_FILE"
+# echo "host=localhost" >> "$CONFIG_FILE"
+# chown vagrant:vagrant "$CONFIG_FILE"
+# chmod 600 "$CONFIG_FILE"
+
+ROOT_CONFIG_FILE="/root/.my.cnf"
+{
+    echo "[client]"
+    echo "user=root"
+    echo "password=${MYSQL_ROOT_PASSWORD}"
+    echo "host=localhost"
+} > "$ROOT_CONFIG_FILE"
+sudo chmod 600 "$ROOT_CONFIG_FILE"
+CONFIG_FILE="/home/vagrant/.my.cnf"
+{
+    echo "[client]"
+    echo "user=root"
+    echo "password=${MYSQL_ROOT_PASSWORD}"
+    echo "host=localhost"
+} > "$CONFIG_FILE"
+sudo chown vagrant:vagrant "$CONFIG_FILE"
+sudo chmod 600 "$CONFIG_FILE"
+
 # Создание базы данных
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS my_database;"
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;"
+sudo mysql --defaults-extra-file="$CONFIG_FILE" -e "CREATE DATABASE IF NOT EXISTS my_database;"
 
 # Создание таблиц и заполнение данными
-mysql -u root -e "
+sudo mysql --defaults-extra-file="$CONFIG_FILE" -e "
 USE my_database;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -37,6 +76,7 @@ INSERT INTO users (name, email) VALUES ('Petro', 'petro@example.by');
 # Создание директории для бэкапов
 sudo mkdir -p /opt/mysql_backup
 sudo chown -R vagrant:vagrant /opt/mysql_backup
+sudo chmod -R g+w /opt/mysql_backup
 
 # Создание скрипта для бэкапа
 echo '#!/bin/bash' > /home/vagrant/mysql_backup.sh
@@ -48,3 +88,4 @@ chmod +x /home/vagrant/mysql_backup.sh
 # (crontab -l 2>/dev/null; echo "0 * * * * /home/vagrant/mysql_backup.sh") | crontab -
 # Добавление под vagrant
 sudo -u vagrant sh -c '(crontab -l 2>/dev/null; echo "0 * * * * /home/vagrant/mysql_backup.sh") | crontab -'
+sudo -u vagrant sh -c '(crontab -l 2>/dev/null; echo "*/1 * * * * /home/vagrant/mysql_backup.sh") | crontab -' # для теста каждую минуту
--- lesson9/sh_provision/store.sh
+++ lesson9/sh_provision/store.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "store provision!"
+
 sudo apt-get update
 sudo apt-get install -y rsync
 
@@ -9,9 +10,10 @@ sudo chown -R vagrant:vagrant /opt/store/mysql
 
 # Настройка синхронизации данных с сервера
 echo "#!/bin/bash" > /home/vagrant/sync_mysql.sh
-echo "rsync -avz --delete vagrant@192.168.50.200:/opt/mysql_backup/ /opt/store/mysql/" >> /home/vagrant/sync_mysql.sh
+echo 'rsync -avz -e "ssh -i /home/vagrant/.ssh/vagrant_vm_rsa" vagrant@192.168.50.200:/opt/mysql_backup/ /opt/store/mysql/' >> /home/vagrant/sync_mysql.sh
 chmod +x /home/vagrant/sync_mysql.sh
 
 # Настройка cron для запуска синхронизации каждый час
 #(crontab -l 2>/dev/null; echo "0 * * * * /home/vagrant/sync_mysql.sh") | crontab -
 sudo -u vagrant sh -c '(crontab -l 2>/dev/null; echo "0 * * * * /home/vagrant/mysql_backup.sh") | crontab -'
+sudo -u vagrant sh -c '(crontab -l 2>/dev/null; echo "*/1 * * * * /home/vagrant/mysql_backup.sh") | crontab -' # для теста каждую минуту
